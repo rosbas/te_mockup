80bf3a1756117f251448c2b39a24173713ab7e24
Rosbas <panutatr@hotmail.com>
2021-07-08 09:46:27 +0700

    Added the confirm popup for calendar
    Added the confirm popup for calendar.

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,22 @@
     <div class='max-w-sm -mt-6 mb-10 w-full h-auto justify-center bg-white shadow-xl rounded-lg overflow-hidden mx-auto flex flex-col p-5 space-y-5'>
       <Name/>
       <Date/>
+      <!-- <button class="px-6 py-4 m-auto bg-pink-200 rounded-xl text-white shadow-lg hover:bg-pink-900" @click="toggleDateModal = !toggleDateModal">{{toggleDateModal}}</button> -->
+      <!-- <div @click="toggleDateModal = !toggleDateModal" v-if="toggleDateModal" class="absolute z-40 inset-0 opacity-25 bg-black"></div> -->
       <Seat/>
-      <button @click="onSubmited" class="py-6 px-20 mx-auto max-w-md rounded-xl shadow-md bg-red-400 flex items-center">จองคิว</button>
+      <!-- <button @click="onSubmited" class="py-6 px-20 mx-auto max-w-md rounded-xl shadow-md bg-red-400 flex items-center">จองคิว</button> -->
     </div>
+    <!-- <div v-if="toggleDateModal" class="fixed overflow-x-hidden overflow-y-auto inset-0">
+      <button @click="toggleDateModal = !toggleDateModal">Hi</button>
+      <CalendarMonth></CalendarMonth>
+    </div> -->
     <ConfirmPopup></ConfirmPopup>
   </div>
 </template>
 
 <script>
 import ConfirmPopup from '../components/ConfirmPopup.vue'
+import CalendarMonth from '../components/CustomCalendar/CalendarMonth.vue'
 
   export default {
     // computed:{
@@ -27,15 +34,14 @@ import ConfirmPopup from '../components/ConfirmPopup.vue'
     //   },
     // },
     components:{
-      ConfirmPopup
+      ConfirmPopup,
+      CalendarMonth
     },
-    //data() {
-      //return {
-        //popupData : {
-          //"display" : "none"
-        //}
-      //}
-    //},
+    data() {
+      return {
+        toggleDateModal: false
+      }
+    },
     methods:{
       onSubmited(){
         var data = {
@@ -48,7 +54,7 @@ import ConfirmPopup from '../components/ConfirmPopup.vue'
         //show confirmation modals
         //this.popupData.display = "block";
         this.$store.commit('changeConfirmpopupDisplay',"block")
-      },
+      }
       //mounted(){
         //this.popupData.display = "block";
       //}
